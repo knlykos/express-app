83a27f47fe9e577dec2eef1a476e10a90d790be1
Nefi López García <nlopezg87@gmail.com>
2019-04-12 08:37:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,25 @@ var app = express();
 
 app.set("view engine", "ejs");
 
+// Creamos el index de la página web
+// req y res, son paremetros en una función, dentro de otra función, llamada callback. [investiga]
+// req, significa request, lo que el navegador envía a nuestro servidor web
+// res, significa response, lo que el servidor envía al navegador
 app.get("/", function(req, res) {
+  
+  
+  // Este bloque representa la lógica que se puede efectuar,
+  // Muchas veces podemos recibir información de un usuario de formularios
+  // y esa información necesitamos procesarla,
+  // Por ahora concentrate en aprender esto muy basico
   var contador = 2 + 2;
-  res.render("index", { variable: "Hola", contador });
+  
+  
+  
+// Investiga objetos javascript { variable: "Hola", contador },
+// pasar parametros en funciones
+// en este caso estamos pasando un objeto de javascript como parametro
+  res.render("index", { variable: "Hola", contador }); 
 });
 
 app.get("/estrenos", function(req, res) {
